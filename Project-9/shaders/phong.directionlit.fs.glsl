d3c6fa9cb7963b9fe68b7c34cc93c94614c43b56
ConlynPattison <cpattison@csumb.edu>
2022-11-09 18:46:51 -0800

    create final color with ambient + diffuse + phong

--- Project-9/shaders/phong.directionlit.fs.glsl
+++ Project-9/shaders/phong.directionlit.fs.glsl
@@ -27,21 +27,24 @@ void main(void) {
 
     // combine
     // todo #7 apply light and material interaction for diffuse value by using the texture color as the material
+    vec3 materialLight = texture2D(uTexture, vTexcoords).rgb;
+    materialLight = materialLight * lambertTerm;
     // todo #8 apply light and material interaction for phong, assume phong material color is (0.3, 0.3, 0.3)
+    vec3 materialLightPhong = vec3(0.3, 0.3, 0.3) * phongTerm;
 
 
     vec3 albedo = texture2D(uTexture, vTexcoords).rgb;
 
     vec3 ambient = albedo * 0.1;
-    // vec3 diffuseColor = todo
-    // vec3 specularColor = todo
+    vec3 diffuseColor = materialLight;
+    vec3 specularColor = materialLightPhong;
 
     // todo #9
     // add "diffuseColor" and "specularColor" when ready
-    vec3 finalColor = ambient; // + diffuseColor + specularColor;
+    vec3 finalColor = ambient + diffuseColor + specularColor;
 
     //gl_FragColor = vec4(finalColor, 1.0);
-    gl_FragColor = vec4(normalizedReflectionVector, 1.0);
+    gl_FragColor = vec4(finalColor, 1.0);
 }
 
 // EOF 00100001-10
