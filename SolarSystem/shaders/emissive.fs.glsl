670a6c25c93b1a6c66e19aa4f43b29c83bc24d1b
ConlynPattison <cpattison@csumb.edu>
2022-12-07 17:17:41 -0800

    v1 of emissive non-uniform variance

--- SolarSystem/shaders/emissive.fs.glsl
+++ SolarSystem/shaders/emissive.fs.glsl
@@ -7,8 +7,19 @@ varying vec2 vTexcoords;
 varying vec3 vWorldPosition;
 
 void main(void) {
-    vec3 albedo = texture2D(uTexture, vTexcoords).rgb;
-    gl_FragColor = vec4(albedo.x, albedo.y, albedo.z, 1.0);
+    float speedFactor = 45.0;
+    float brightFactor = 0.17;
+    float periodFactor = 0.20;
+
+    float hStripe = (cos((vWorldPosition.x + (uTime * speedFactor)) * periodFactor) + 5.0) * brightFactor;
+    float vStripe = (sin((vWorldPosition.y + (uTime * speedFactor)) * periodFactor)+ 5.0) * brightFactor;
+    float zStripe = (cos((vWorldPosition.z + (uTime * speedFactor)) * periodFactor) + 5.0) * brightFactor;
+
+    float checker = hStripe * vStripe * zStripe;
+
+    vec3 albedo = texture2D(uTexture, vTexcoords.xy).rgb;
+
+    gl_FragColor = vec4(albedo + albedo*checker, 1.0);
 }
 
 // EOF 00100001-10
